d40e2dbe16c4809f28692cc386530156da8381ef
cricritoia <toiacristiandaniel@gmail.com>
2024-07-17 21:43:52 -0300

    primera versión con react

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-gray-600">
+        Wallock con React Typescript Tailwind
+      </h1>
+    </>
+  )
+}
+
+export default App
