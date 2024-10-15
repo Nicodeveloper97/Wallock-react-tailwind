653339aef6326b4027c807c8d50090cf0e3fdb6d
cristiantoia-wallock <ctoia@wallock.com.ar>
2024-10-15 10:03:59 -0300

    cambio en tiempo de animación de carrusel

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,64 +1,4 @@
-// import { useEffect, useRef } from "react";
-
-// const Slider: React.FC = () => {
-//   const sliderRef = useRef<HTMLDivElement>(null);
-
-//   useEffect(() => {
-//     const slider = sliderRef.current;
-//     if (!slider) return;
-
-    
-//     slider.style.animation = "scroll 30s linear infinite";
-
-//     const style = document.createElement("style");
-//     style.innerHTML = `
-//       @keyframes scroll { 
-//       0% { transform: translateX(0); }
-//       100% { transform: translateX(-50%); } }`;
-//     document.head.appendChild(style);
-
-//     return () => {
-//       document.head.removeChild(style);
-//     };
-//   }, []);
-
-//   const items = [
-//     "Sistemas Internos",
-//     "Consultorías",
-//     "Integraciones",
-//     "E-commerce",
-//     "Websites",
-//     "Apps Nativas",
-//     "Web Apps",
-//   ];
-
-//   return (
-//     <section className="relative w-full h-[80px] md:h-[111px] bg-[#F2F2F7]  flex items-center justify-center overflow-hidden z-10">
-      
-//       <div
-//         ref={sliderRef}
-//         className="w-[200%] flex items-center gap-[12px] md:gap-[22px] whitespace-nowrap"
-//       >
-        
-//         {[...items, ...items].map((item, index) => (
-//           <div className="flex items-center" key={index}>
-//             <div className="flex-none px-2 md:px-4 py-1 md:py-2 text-[#000000] shadow-[0px_0.92px_0.92px_0px_rgb] text-left font-quicksand text-[20px] md:text-[32px] font-medium leading-[28px] md:leading-[40px] whitespace-nowrap">
-//               {item}
-//             </div>
-            
-//             {index < [...items, ...items].length - 1 && (
-//               <div className="flex-none w-[8px] md:w-[12px] h-[8px] md:h-[12px] bg-[#4361EE] rounded-full mx-1 md:mx-2" />
-//             )}
-//           </div>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Slider;
-
-
+import '../index.css'
 import React from 'react';
 
 const items = [
@@ -85,7 +25,7 @@ const icons = [
 const Carousel: React.FC = () => {
   return (
     <div className="overflow-hidden whitespace-nowrap py-12 bg-[#F2F2F7]">
-      <div className="animate-marquee inline-flex">
+      <div className="animate-marquee inline-flex scroll">
         {[...items, ...items, ...items, ...items].map((item, index) => (
           <div key={index} className="inline-block mx-8">
             <p className="inline-flex">{icons[index % icons.length]}<span className='inline-flex mt-5 mr-3 font-quicksand font-bold'>{item}</span></p>
